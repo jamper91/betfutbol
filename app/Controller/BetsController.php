25b1751b9300311352b27fafff433ed774207780
Jorge Moreno <jamper91@hotmail.com>
2014-09-09 22:55:12 -0500

    no message

--- app/Controller/BetsController.php
+++ app/Controller/BetsController.php
@@ -319,6 +319,7 @@ class BetsController extends AppController {
             "conditions"=>array(
                 "DATE_FORMAT(Bet.created, '%Y-%m-%d')"=>$fecha,
                 "vendedor_id"=>$idUsuario
+                "Bet.valido"=>"1"
             ),
             "group"=>array(
                 "DATE_FORMAT(Bet.created, '%Y-%m-%d')"
